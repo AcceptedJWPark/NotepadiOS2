e71d6f67aeb511fb8a229460baee542e6dc8774c
박종우 <pjw910123@gmail.com>
2020-10-12 17:43:08 +0900

    매뉴얼 작업

---
+++ Notepad/PaperViewController.swift
@@ -0,0 +1,63 @@
+//
+//  PaperViewController.swift
+//  Notepad
+//
+//  Created by Accepted on 2020/10/12.
+//  Copyright © 2020 Accepted. All rights reserved.
+//
+
+import UIKit
+
+class PaperViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet var headerContainer: UIView!
+    @IBOutlet var headerTitle: UILabel!
+    @IBOutlet var headerBack: UIImageView!
+    
+    
+    @IBOutlet var btnSecurity: UIButton!
+    
+    @IBOutlet var paperContents: UITextView!
+    @IBOutlet var paperTitle: UITextField!
+    
+    @IBOutlet var saveBtn: UIButton!
+    
+    @IBOutlet var totalContainer: UIView!
+    
+    var placeholderLabel : UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        saveBtn.layer.cornerRadius = 10
+        headerContainer.layer.shadowColor = UIColor.gray.cgColor
+             headerContainer.layer.shadowOpacity = 0.5
+             headerContainer.layer.shadowRadius = 3
+             headerContainer.layer.shadowOffset = CGSize(width: 1, height: 1)
+        
+        totalContainer.backgroundColor = UIColor(hexFromString: "#f5f5f5")
+        headerContainer.backgroundColor = UIColor(hexFromString: "#ffffff")
+        headerBack.tintColor = UIColor(hexFromString: "#544f4f")
+        saveBtn.backgroundColor = UIColor(hexFromString: "#f37a00")
+        btnSecurity.setTitleColor(UIColor(hexFromString: "#f37a00"), for: .normal)
+        
+        paperTitle.attributedPlaceholder = NSAttributedString(string: "제목을 입력해주세요",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor(hexFromString: UIColor(hexFromString: "#544f4f"))])
+        
+        
+        paperContents.delegate = self
+        placeholderLabel = UILabel()
+        placeholderLabel.text = "내용을 입력해주세요"
+        paperContents.font = UIFont.systemFont(ofSize: paperContents.font!.pointSize)
+        placeholderLabel.sizeToFit()
+        paperContents.addSubview(placeholderLabel)
+        paperContents.frame.origin = CGPoint(x: 5, y: (paperContents.font?.pointSize)! / 2)
+        placeholderLabel.textColor = UIColor(hexFromString: "#544f4f")
+        paperContents.isHidden = !paperContents.text.isEmpty
+        
+    }
+    
+
+
+}
